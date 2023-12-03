9b9ae8bb685a1f868921585780e7bfee34f1eee1
Aditi Jain <jaditi930@gmail.com>
2023-12-04 01:16:50 +0530

    bug fixed

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function toggle_ans(id)
 {
-
 let ans=document.getElementById(`ans_${id}`);
-console.log(ans)
 
-    if(ans.state=="off")
+    if(ans.dataset.state==="off")
     {
-        ans.style.display="block";
-        ans.state="on";
+        ans.classList.remove("hide")
+        ans.dataset.state="on";
         document.getElementById(`img_${id}`).src="./assets/images/icon-minus.svg"
     }
     else
     {
-        ans.style.display="none";
-        ans.state="off";
+        ans.classList.add("hide")
+        ans.dataset.state="off";
         document.getElementById(`img_${id}`).src="./assets/images/icon-plus.svg"
     }
 
